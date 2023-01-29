e2086af5a73a845a5f7845ef0a53f8846335dbb7
John Elmo Johnson <elmoj6@gmail.com>
2023-01-29 13:46:56 +1100

    Display message when anime list is empty

--- src/components/lists/AnimeList.tsx
+++ src/components/lists/AnimeList.tsx
@@ -2,6 +2,7 @@ import { Anime } from "../../interface";
 import AnimeItem from "../items/AnimeItem";
 import { motion } from "framer-motion";
 import AnimeItemLoading from "../items/AnimeItemLoading";
+import { useLocation } from "react-router-dom";
 
 interface List {
   list: Anime[];
@@ -9,6 +10,7 @@ interface List {
 }
 
 const AnimeList = ({ list, isLoading }: List) => {
+  const location = useLocation();
   return (
     <>
       {isLoading ? (
@@ -16,25 +18,28 @@ const AnimeList = ({ list, isLoading }: List) => {
           initial={{ opacity: 0, y: 10 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 10 }}
-          transition={{delay: 0.2}}
+          transition={{ delay: 0.2 }}
           className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8"
         >
           {[...Array(15)].map((_, i) => {
-            return <AnimeItemLoading key={i} />
+            return <AnimeItemLoading key={i} />;
           })}
         </motion.div>
       ) : (
-        <motion.div
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 10 }}
-          transition={{delay: 0.2}}
-          className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8"
-        >
-          {list.map((anime: Anime) => {
-            return <AnimeItem key={anime.mal_id} {...anime} />;
-          })}
-        </motion.div>
+        <>
+          {list.length === 0 && location.pathname !== "/search" ? <p className="text-white">No data available</p> : null}
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 10 }}
+            transition={{ delay: 0.2 }}
+            className="grid 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8"
+          >
+            {list.map((anime: Anime) => {
+              return <AnimeItem key={anime.mal_id} {...anime} />;
+            })}
+          </motion.div>
+        </>
       )}
     </>
   );
