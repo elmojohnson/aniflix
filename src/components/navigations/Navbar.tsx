f0c2fc10f3ae76308b70f7a6a37080888cb3fcc1
John Elmo Johnson <elmoj6@gmail.com>
2023-02-01 01:02:06 +1100

    Changed navbar title color

--- src/components/navigations/Navbar.tsx
+++ src/components/navigations/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper flex items-center justify-between">
-        <NavLink to="/" className="font-bold text-2xl">Aniflix</NavLink>
+        <NavLink to="/" className="font-bold text-3xl uppercase tracking-wider text-primary">Aniflix</NavLink>
         <NavLink to="/search" className="text-xl text-muted hover:text-white"><MdSearch /></NavLink>
       </div>
     </div>
